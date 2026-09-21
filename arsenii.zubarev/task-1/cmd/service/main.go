677ed45c45a938b553fa8ad60b4eccf25abd8df0
zub <ars1for1work@gmail.com>
2026-09-21 19:16:33 +0300

    [TASK-1] complete task with solve

--- arsenii.zubarev/task-1/cmd/service/main.go
+++ arsenii.zubarev/task-1/cmd/service/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var num1 int
+	_, err1 := fmt.Scan(&num1)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var num2 int
+	_, err2 := fmt.Scan(&num2)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var sign string
+	_, errSign := fmt.Scan(&sign)
+	if errSign != nil || len(sign) != 1 {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	var result int
+	result = num1
+
+	switch sign {
+	case "+":
+		result += num2
+	case "-":
+		result -= num2
+	case "*":
+		result *= num2
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		}
+		result /= num2
+	default:
+		fmt.Printf("Unrecognized sign: %c\n", sign)
+
+	}
+	fmt.Println(result)
+
 }
